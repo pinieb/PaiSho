f372fb9bd1002d3b450fbb3794eadeda09ad36ac
pinieb <petebiencourt@gmail.com>
2014-07-09 14:04:27 -0700

    Basic turn functionality

---
+++ Assets/Scripts/EndTurnButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTurnButton : MonoBehaviour 
+{
+	public GameManager gameManager;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseUp()
+	{
+		gameManager.EndTurn();
+	}
+}
